fcac52fea99303c96ee796bfb1e6d324699b3ed5
toreejelly <toreejelly@gmail.com>
2023-08-18 15:22:42 +0900

    프로젝트 생성

---
+++ boardTestThymeleaf/src/main/java/com/example/demo/BoardTestThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTestThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardTestThymeleafApplication.class, args);
+	}
+
+}
